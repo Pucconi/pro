95e55f8ffa3de29cf112a0767d5d8a88ddaa1120
Pucconi <83922260+Pucconi@users.noreply.github.com>
2021-05-09 22:47:31 +0200

    Add project files.

---
+++ project/Window1.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace project
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+            string Kraj = Label_LAB1.text;
+            string Miasto = Label.Content;
+            string Hotel = Label.Content;
+            string Zabytek = Label.Content;
+            try
+            {
+                StringBuilder adres = new StringBuilder();
+                adres.Append("https://www.google.pl/maps/?q=");
+                if (Kraj != string.Empty)
+                {
+                    adres.Append(Kraj + "," + "+");
+                }
+                if (Miasto != string.Empty)
+                {
+                    adres.Append(Miasto + "," + "+");
+                }
+                if (Hotel != string.Empty)
+                {
+                    adres.Append(Hotel + "," + "+");
+                }
+                if (Zabytek != string.Empty)
+                {
+                    adres.Append(Zabytek + "," + "+");
+                }
+                webBrowser1.Navigate(adres.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }
+        }
+    }
+}
+
+
