60d8567e4f83711ea79ddc74e464e7c7dbbe99f6
Pucconi <83922260+Pucconi@users.noreply.github.com>
2021-05-10 12:32:58 +0200

    hotele i restauracje

--- project/Window1.xaml.cs
+++ project/Window1.xaml.cs
@@ -34,30 +34,30 @@ namespace project
         private void butt1_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
             webBrowser1.Visibility = Visibility.Visible;
-            string Kraj = LAB1.Text;
+           // string Kraj = LAB1.Text;
             string Miasto = LAB1.Text;
-            string Hotel = LAB1.Text;
-            string Zabytek = LAB1.Text;
+           // string Hotel = LAB1.Text;
+           // string Zabytek = LAB1.Text;
             try
             {
                 StringBuilder adres = new StringBuilder();
                 adres.Append("https://www.bing.com/maps?q=");
-                if (Kraj != string.Empty)
-                {
-                    adres.Append(Kraj + "," + "+");
-                }
+               // if (Kraj != string.Empty)
+               // {
+               //     adres.Append(Kraj + "," + "+");
+              //  }
                 if (Miasto != string.Empty)
                 {
                     adres.Append(Miasto + "," + "+");
                 }
-                if (Hotel != string.Empty)
-                {
-                    adres.Append(Hotel + "," + "+");
-                }
-                if (Zabytek != string.Empty)
-                {
-                    adres.Append(Zabytek + "," + "+");
-                }
+               // if (Hotel != string.Empty)
+               // {
+               //     adres.Append(Hotel + "," + "+");
+               // }
+               // if (Zabytek != string.Empty)
+               // {
+               //     adres.Append(Zabytek + "," + "+");
+               // }
                 webBrowser1.Navigate(adres.ToString());
             }
             catch (Exception ex)
@@ -71,6 +71,58 @@ namespace project
         {
             webBrowser1.Visibility = Visibility.Hidden;
             trya.Visibility = Visibility.Hidden;
+        }
+
+        private void butt2_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            webBrowser1.Visibility = Visibility.Visible;
+            string Kraj = LAB1.Text;
+            string hotel = "hotele";
+            try
+            {
+                StringBuilder adres = new StringBuilder();
+                adres.Append("https://www.bing.com/maps?q=");
+                if (Kraj != string.Empty)
+                {
+                    adres.Append(Kraj + "," + "+");
+                }
+                if (hotel != string.Empty)
+                {
+                    adres.Append(hotel + "," + "+");
+                }
+                webBrowser1.Navigate(adres.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }
+            trya.Visibility = Visibility.Visible;
+        }
+
+        private void butt3_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            webBrowser1.Visibility = Visibility.Visible;
+            string Kraj = LAB1.Text;
+            string res = "restaurant";
+            try
+            {
+                StringBuilder adres = new StringBuilder();
+                adres.Append("https://www.bing.com/maps?q=");
+                if (Kraj != string.Empty)
+                {
+                    adres.Append(Kraj + "," + "+");
+                }
+                if (res != string.Empty)
+                {
+                    adres.Append(res + "," + "+");
+                }
+                webBrowser1.Navigate(adres.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error");
+            }
+            trya.Visibility = Visibility.Visible;
         }
     }
 }
