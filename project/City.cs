b270b65f7bc68e6e994d43ad9c75c551bb989a2b
Paulina Dworaczyk <pauldwo861@student.polsl.pl>
2021-05-10 11:35:11 +0200

    implementacja klasy city z odpowiednimi właściwościami oraz utworzenie migracji do utworzenia bazy danych, oraz utworzenie ConnectionStringa umożliwiającego to

---
+++ project/City.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    public class City
+    {
+        //tu bedzie polaczenie z baza 
+
+        public string Description { get; set; }
+        public List<string> Places { get; set; }
+        public string ImageLink { get; set; }
+        public bool Favourites { get; set; }
+        public string Transport { get; set; }
+        public string Hotels { get; set; }
+        public string Restaurants { get; set; }
+        public int CityId { get; set; }
+        //zapytanie do bazy danych
+        public string Name { get; set; }
+        public City(string name)
+        {
+
+
+
+        }
+    }
+}
---
+++ project/Migrations/202105092308200_Migr.cs
@@ -0,0 +1,32 @@
+﻿namespace project.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Migr : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Cities",
+                c => new
+                    {
+                        CityId = c.Int(nullable: false, identity: true),
+                        Description = c.String(),
+                        ImageLink = c.String(),
+                        Favourites = c.Boolean(nullable: false),
+                        Transport = c.String(),
+                        Hotels = c.String(),
+                        Restaurants = c.String(),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.CityId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Cities");
+        }
+    }
+}
